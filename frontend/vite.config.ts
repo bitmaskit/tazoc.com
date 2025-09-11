8aa2b11b7a5be660d4fc6f58cf36714dbee888de
Valentin L <valentin@bitmask.it>
2025-09-12 00:49:55 +0300

    fix tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), cloudflare()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), cloudflare()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
