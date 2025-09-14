e037c10bbb43a9e2f438ee56656fa273d366a99e
Valentin L <valentin@bitmask.it>
2025-09-14 19:37:02 +0300

    fix

--- app-frontend/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.CXl-zFkh.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CZg8cpZA.js","_app/immutable/chunks/DoRsGw_t.js"];
-export const stylesheets = ["_app/immutable/assets/0.CwzivqHL.css"];
-export const fonts = [];
