6975ed00ba0234973c31e43fd802803773875bc5
Valentin L <valentin@bitmask.it>
2025-09-14 23:02:54 +0300

    remove unneeded worker

--- landing-worker/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { handleRequest } from './handler';
-import { Env } from './types';
-
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return handleRequest(request, env, ctx);
-  },
-};
